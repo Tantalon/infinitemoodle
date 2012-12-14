cabfcdadcf49fcd278c7e0d48753a1a11d65ada9
Robert Egglestone <r.egglestone@auckland.ac.nz>
2012-12-14 23:50:55 +1300

    Rename test files

---
+++ now.php
@@ -0,0 +1,10 @@
+<?php
+
+// Run the export immediately, useful for testing
+
+require(dirname(__FILE__).'/../../config.php');
+require_once($CFG->libdir.'/adminlib.php');
+require_once('lib.php');
+
+report_infiniterooms_cron();
+
--- ping.php
+++ ping.php
@@ -1,14 +1,11 @@
 <?php
 
-// Run the export immediately, useful for testing
+// Ping the remote API, useful for testing
 
 require(dirname(__FILE__).'/../../config.php');
 require_once($CFG->libdir.'/adminlib.php');
 require_once('lib.php');
 
-//report_infiniterooms_cron();
-
 header('Content-Type: text/plain');
-print report_infiniterooms_remote('GET', 'requestinfo.php');
-
+print report_infiniterooms_remote('GET', 'ping/authenticated');
 
